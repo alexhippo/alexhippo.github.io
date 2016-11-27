3b665ee6e2df092fe5f22c77d721bb9d242de0f8
alexhippo <alexheepo@gmail.com>
2016-11-28 09:51:42 +1100

    Updates after running site through HTML validator

--- files/activescroll.js
+++ files/activescroll.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
 
 //After scrolling 100px from the top...
-if ( $(window).scrollTop() >= 600 ) {
+if ( $(window).scrollTop() >= 300 ) {
     $('#nav').css('background-color', '#000');
 
 //Otherwise remove inline styles and thereby revert to original stying
