3bf051bc48a8483bcdab7c1383088ab4a91a5ab4
alexhippo <alexheepo@gmail.com>
2016-11-30 17:11:44 +1100

    US4 - implement design

--- projects/jscalculator/calc.js
+++ projects/jscalculator/calc.js
@@ -9,19 +9,24 @@ function input(id) {
         return false;
     } else if ((document.getElementById("display").value == 0) && (id === "decimal")) {
         document.getElementById("display").value = "0.";
+        document.getElementById("result").value = "0.";
     } else if ((document.getElementById("display").value == "0") && (document.getElementById(id).className == "number")) { //don't apply operators if initial value is 0. if decimal add 0
         document.getElementById("display").value = document.getElementById(id).value;
+        document.getElementById("result").value = document.getElementById(id).value;
     } 
     
     else {
         //if last character is an operator - replace it
         if ((/(-$)|(\+$)|(\/$)|(\*$)/gm.test(document.getElementById("display").value)) && (id === "multiply" || id === "divide" || id === "plus" || id === "minus" )){
             document.getElementById("display").value = document.getElementById("display").value.replace(/(-$)|(\+$)|(\/$)|(\*$)/gm, document.getElementById(id).value);
+            document.getElementById("result").value = document.getElementById("result").value.replace(/(-$)|(\+$)|(\/$)|(\*$)/gm, document.getElementById(id).value);
         } else {
             if ((id === "decimal") && (document.getElementById("display").value.split('.').length === 2)) { //if there is already a decimal point, do not put another decimal point
                 return false;
             } else {
                 document.getElementById("display").value += document.getElementById(id).value; 
+                document.getElementById("result").value += document.getElementById(id).value; 
+                
             }
         } 
     }
