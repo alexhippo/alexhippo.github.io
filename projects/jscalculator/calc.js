9816ebb370a8dbc87da9d230c6200fffe1195282
alexhippo <alexheepo@gmail.com>
2016-11-30 11:39:09 +1100

    Fixed bug which didn't display a leading 0 after pressing 0 and decimal button

--- projects/jscalculator/calc.js
+++ projects/jscalculator/calc.js
@@ -1,7 +1,3 @@
-//To-Do: variables for display and result
-//blank display
-
-
 function reset() {
     document.getElementById("display").value = "0";
     document.getElementById("result").value = "0";
@@ -11,9 +7,13 @@ function input(id) {
     //no multiple 0s
     if ((document.getElementById("display").value == 0) && (document.getElementById(id).className !== "number"))    {
         return false;
-    } else if ((document.getElementById("display").value == 0) && (document.getElementById(id).className == "number")) { //don't apply operators if initial value is 0
+    } else if ((document.getElementById("display").value == 0) && (id === "decimal")) {
+        document.getElementById("display").value = "0.";
+    } else if ((document.getElementById("display").value == "0") && (document.getElementById(id).className == "number")) { //don't apply operators if initial value is 0. if decimal add 0
         document.getElementById("display").value = document.getElementById(id).value;
-    } else {
+    } 
+    
+    else {
         //if last character is an operator - replace it
         if ((/(-$)|(\+$)|(\/$)|(\*$)/gm.test(document.getElementById("display").value)) && (id === "multiply" || id === "divide" || id === "plus" || id === "minus" )){
             document.getElementById("display").value = document.getElementById("display").value.replace(/(-$)|(\+$)|(\/$)|(\*$)/gm, document.getElementById(id).value);
