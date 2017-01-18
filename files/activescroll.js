228b83f282e41b1e2f0d03599a982085f8369bb5
alexhippo <alexheepo@gmail.com>
2017-01-18 14:28:16 +1100

    Fixed general styling and colours of the portfolio website

--- files/activescroll.js
+++ files/activescroll.js
@@ -2,7 +2,7 @@ $(window).scroll(function() {
 
 //After scrolling 100px from the top...
 if ( $(window).scrollTop() >= 300 ) {
-    $('#nav').css('background-color', '#000');
+    $('#nav').css('background-color', '#282826');
 
 //Otherwise remove inline styles and thereby revert to original stying
 } else {
