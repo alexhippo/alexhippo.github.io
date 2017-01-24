aeb88281837d3063c305f7ecafb518ed7e417176
alexhippo <alexheepo@gmail.com>
2017-01-24 18:09:14 +1100

    Code refactoring and HTML/CSS validation fixes

--- projects/simongame/simongame.js
+++ projects/simongame/simongame.js
@@ -1,262 +1,269 @@
-//disable double click to prevent multiple timers and series
-document.getElementsByTagName("div").ondblclick = function() { 
-    return false; 
-}
-
-
-var colours = ["green", "red", "yellow", "blue"],
-    series = [],
-    playersTurn = false,
-    checkpoint = 0,
-    timeToChoose = 0,
-    n = 0,
-    gameInProgress = false,
-    strictMode = false,
-    greenSound = document.getElementById("greenSound"),
-    redSound = document.getElementById("blueSound"),
-    yellowSound = document.getElementById("yellowSound"),
-    blueSound = document.getElementById("blueSound"),
-    wrongSound = document.getElementById("wrongSound"),
-    victorySound = document.getElementById("victorySound"),
-    correctSound = document.getElementById("correctSound");
-
-function showInstructions() {
-    document.getElementById("howtoplay").style.visibility = "visible";
-    document.getElementById("overlay").style.visibility = "visible";
-}
-
-function closeInstructions() {
-    document.getElementById("howtoplay").style.visibility = "hidden";
-    document.getElementById("overlay").style.visibility = "hidden";
-}
-
-
-function start() {
-    if ((playersTurn === true) || (gameInProgress === true)) {
-        //disabling button if play is in progress
-        return false;
-    } else {
-        chooseAndPresentSeries();
-        gameInProgress = true;
+var game = {
+    colours: ["green", "red", "yellow", "blue"],
+    series: [],
+    playersTurn: false,
+    checkpoint: 0,
+    timeToChoose: 0,
+    time: 0,
+    gameInProgress: false,
+    strictMode: false,
+    victory: false,
+    sound: {
+        greenSound: document.getElementById("greenSound"),
+        redSound: document.getElementById("redSound"),
+        yellowSound: document.getElementById("yellowSound"),
+        blueSound: document.getElementById("blueSound"),
+        correctSound: document.getElementById("correctSound"),
+        wrongSound: document.getElementById("wrongSound"),
+        victorySound: document.getElementById("victorySound")
     }
-    buttonPress("start");
-}
+};
 
-function setStrictMode(status) {
-    strictMode = status;
-    if (status === true) {
-        document.getElementById("strict").innerHTML = '<i class="fa fa-toggle-on" aria-hidden="true" onclick="setStrictMode(false);"></i>'
-    } else {
-        document.getElementById("strict").innerHTML = '<i class="fa fa-toggle-off" aria-hidden="true" onclick="setStrictMode(true);"></i>'
-    }
-}
-
-function chooseAndPresentSeries() {
-    chooseSeriesColour(); 
-    presentSeries(); 
-}
-
+//Animation/Sounds/Styling
 function buttonPress(id) {
-    document.getElementById(id).className = "titlesquare";
+    "use strict";
+    document.getElementById(id).className = "navbarcell";
     
-    setTimeout(function() {
-      document.getElementById(id).className = "dark titlesquare";
-        }, 500);
+    setTimeout(function () {
+        document.getElementById(id).className = "dark navbarcell";
+    }, 500);
 }
 
-function timeLimit() {
-    timeToChoose = setInterval(function(){
-        n++;
-        if (n === 3) {
-            check("wrong");
-            clearInterval(timeToChoose);
-            n = 0;
-        }
-    }, 1000)
+function darkColour(id) {
+    "use strict";
+    document.getElementById(id).className = "dark square";
 }
 
-
-function reset() {
-    if (series.length == 0) {
-        return false;
-    } else {
-        series.length = 0;
-        chooseSeriesColour();
-        checkpoint = 0;
-        setTimeout(presentSeries, 500); 
-        clearInterval(timeToChoose);
-        n = 0;
-        buttonPress("reset");
+function lightColour(id) {
+    "use strict";
+    document.getElementById(id).className = "square";
+    
+    switch (id) {
+    case "green":
+        game.sound.greenSound.play();
+        break;
+    case "red":
+        game.sound.redSound.play();
+        break;
+    case "yellow":
+        game.sound.yellowSound.play();
+        break;
+    case "blue":
+        game.sound.blueSound.play();
+        break;
     }
     
-}
-
-function playerPressColour(id) {
-    if ((playersTurn === false) || (n === 3)) {
-        return false;
-    } else {
-        console.log("I pressed this colour");
-        clearInterval(timeToChoose);
-        check(id);   
-        n = 0;
-        lightColour(id);
-    }
+    setTimeout(function () {
+        darkColour(id);
+    }, 300);
 }
 
 function bounceSign(scenario) {
+    "use strict";
     document.getElementById(scenario).className = "bounceOutAndUp";
     document.getElementById(scenario).style.visibility = "visible";
     
-    setTimeout(function (){
-        document.getElementById(scenario).className = " "; 
+    setTimeout(function () {
+        document.getElementById(scenario).className -= "bounceOutAndUp";
         document.getElementById(scenario).style.visibility = "hidden";
     }, 1500);
 }
 
-//To-Do: Do not allow for 2nd check
-
-function check(id) {
-    if (playersTurn === false) {
-        return false;
-    } else {
-        if (id === series[checkpoint]) {
-        //correct state
-            if (checkpoint < series.length-1) {
-                //move checkpoint forward if we haven't reached the last colour in the series
-                checkpoint++;
-                playersTurn = true;
-                setTimeout(timeLimit, 200);
-            } else {
-                if (series.length === 20) { //TO-DO: Change to 20
-                    victory();
-                    bounceSign("win");
-                } else {
-                    checkpoint = 0;
-                    playersTurn = false;
-                    setTimeout(chooseAndPresentSeries, 2000);
-                    bounceSign("correct");
-                    setTimeout(function() {
-                        correctSound.play();
-                    }, 500);
-                }
-            }
-        } else {
-        if (strictMode === true) {
-            setTimeout(reset, 1500);
-            clearInterval(timeToChoose);
-            n = 0;
-            playersTurn = false;
-            checkpoint = 0;
-            bounceSign("wrong");
-            wrongSound.play();
-        } else {
-            setTimeout(presentSeries, 1500);
-            clearInterval(timeToChoose);
-            n = 0;
-            playersTurn = false;
-            checkpoint = 0;
-            bounceSign("wrong");
-            wrongSound.play();
-            }
-        }
-        
-        
-    } 
-        
-}
-
-function victory() {
-    victorySound.play();
-    gameInProgress = false;
-    playersTurn = false;
-    checkpoint = 0;
-    setTimeout(winningSeries, 500);
-    setTimeout(function (){
-        victorySound.pause();
-        victorySound.currentTime = 0;  
-        series.length = 0;
-        document.getElementById("count").textContent = 0;
-    }, 8000);
-}
-
-function chooseSeriesColour(){
-    //randomise
-    id = colours[Math.floor(Math.random() * colours.length)];
-    series.push(id);
-    document.getElementById("count").textContent = series.length;
-}
-
 function winningSeries() {
-    for (var i in series) {
-        (function(i){
-            setTimeout(function(){
-                document.getElementById(series[i]).className = "square";
-                setTimeout(function() {
-                document.getElementById(series[i]).className = "dark square";
+    "use strict";
+    var i = 0;
+    for (i = 0; i < game.series.length; i += 1) {
+        (function (i) {
+            setTimeout(function () {
+                document.getElementById(game.series[i]).className = "square";
+                setTimeout(function () {
+                    darkColour(game.series[i]);
                 }, 200);
             }, 300 * i);
         }(i));
-    }  
+    }
 }
 
-function setPlayersTurn(){
-    playersTurn = true;
+//Gameplay
+function setStrictMode(status) {
+    "use strict";
+    game.strictMode = status;
+    buttonPress("strict");
+    if (status === true) {
+        document.getElementById("strict").innerHTML = '<i class="fa fa-toggle-on" aria-hidden="true" onclick="setStrictMode(false);"></i>';
+    } else {
+        document.getElementById("strict").innerHTML = '<i class="fa fa-toggle-off" aria-hidden="true" onclick="setStrictMode(true);"></i>';
+    }
+}
+
+function chooseSeriesColour() {
+    "use strict";
+    var id = game.colours[Math.floor(Math.random() * game.colours.length)];
+    game.series.push(id);
+    document.getElementById("count").textContent = game.series.length;
+}
+
+function timeLimit() {
+    "use strict";
+    game.timeToChoose = setInterval(function () {
+        game.time += 1;
+        if (game.time === 3) {
+            check("wrong");
+            clearInterval(game.timeToChoose);
+            game.time = 0;
+        }
+    }, 1000);
+}
+
+function victory() {
+    "use strict";
+    game.sound.victorySound.play();
+    game.victory = true;
+    game.playersTurn = false;
+    game.checkpoint = 0;
+    setTimeout(winningSeries, 500);
+    setTimeout(function () {
+        game.sound.victorySound.pause();
+        game.sound.victorySound.currentTime = 0;
+        game.series.length = 0;
+        document.getElementById("count").textContent = 0;
+        game.gameInProgress = false;
+        game.victory = false;
+    }, 8000);
 }
 
 function presentSeries() {
-    //light colours in series one at a time
-    var seriesTime = 2000;
-    
-    if (series.length >= 15) {
+    "use strict";
+    var seriesTime = 2000,
+        i = 0;
+    //decrease time in between colours being presented as series length increases
+    if (game.series.length >= 15) {
         seriesTime = 700;
-    } else if (series.length >= 10) {
+    } else if (game.series.length >= 10) {
         seriesTime = 1000;
-    } else if (series.length >= 5) {
+    } else if (game.series.length >= 5) {
         seriesTime = 1500;
-    } 
+    }
     
-    console.log(series); //TO-DO: remove this before final release
-    playersTurn = false;
-    for (var i in series) {
-        (function(i){
-            setTimeout(function(){
-                lightColour(series[i]);
-                if (Number(i) === (series.length-1)) {
-                    setTimeout(function() {
-                        playersTurn = true;
+    game.playersTurn = false;
+    
+    for (i = 0; i < game.series.length; i += 1) {
+        (function (i) {
+            setTimeout(function () {
+                lightColour(game.series[i]);
+                if (Number(i) === (game.series.length - 1)) {
+                    setTimeout(function () {
+                        game.playersTurn = true;
                         timeLimit();
-                    }, 1000);    
+                    }, 1000);
                 }
             }, seriesTime * i);
         }(i));
     }
 }
 
-
-function lightColour(id) {
-    //apply light class
-    document.getElementById(id).className = "square";
-    
-    switch(id) {
-        case "green":
-            greenSound.play();
-            break;
-        case "red":
-            redSound.play();
-            break;
-        case "yellow":
-            yellowSound.play();
-            break;
-        case "blue":
-            blueSound.play();
-            break;
-    }
-    
-    setTimeout(function() {
-       darkColour(id); 
-    }, 300);
+function chooseAndPresentSeries() {
+    "use strict";
+    chooseSeriesColour();
+    presentSeries();
 }
 
-function darkColour(id) {
-      document.getElementById(id).className = "dark square";
+function reset() {
+    "use strict";
+    if ((game.series.length === 0) || (game.gameInProgress === false) || (game.victory === true)) {
+        return false;
+    } else {
+        game.series.length = 0;
+        game.checkpoint = 0;
+        setTimeout(chooseAndPresentSeries, 500);
+        clearInterval(game.timeToChoose);
+        game.time = 0;
+        buttonPress("reset");
+    }
+}
+
+function wrongPlay() {
+    "use strict";
+    clearInterval(game.timeToChoose);
+    game.time = 0;
+    game.playersTurn = false;
+    game.checkpoint = 0;
+    bounceSign("wrong");
+    game.sound.wrongSound.play();
+}
+
+function check(id) {
+    "use strict";
+    if (game.playersTurn === false) {
+        return false;
+    } else {
+        //use checkpoint to check against the last colour pressed and the next point in the series array.
+        if (id === game.series[game.checkpoint]) {
+        //correct - if checkpoint and last colour pressed matches
+            if (game.checkpoint < game.series.length - 1) {
+                game.checkpoint += 1;
+                game.playersTurn = true;
+                setTimeout(timeLimit, 200);
+            } else {
+                if (game.series.length === 20) {
+                    victory();
+                    bounceSign("win");
+                } else {
+                    game.checkpoint = 0;
+                    game.playersTurn = false;
+                    setTimeout(chooseAndPresentSeries, 2000);
+                    bounceSign("correct");
+                    setTimeout(function () {
+                        game.sound.correctSound.play();
+                    }, 500);
+                }
+            }
+        } else {
+        //wrong - if checkpoint and last colour pressed doesn't match
+            if (game.strictMode === true) {
+                setTimeout(reset, 1500);
+                wrongPlay();
+            } else {
+                setTimeout(presentSeries, 1500);
+                wrongPlay();
+            }
+        }
+    }
+}
+
+function start() {
+    "use strict";
+    if ((game.playersTurn === true) || (game.gameInProgress === true)) {
+        return false;
+    } else {
+        chooseAndPresentSeries();
+        game.gameInProgress = true;
+    }
+    buttonPress("start");
+}
+
+function playerPressColour(id) {
+    "use strict";
+    if ((game.playersTurn === false) || (game.time === 3)) {
+        return false;
+    } else {
+        clearInterval(game.timeToChoose);
+        check(id);
+        game.time = 0;
+        lightColour(id);
+    }
+}
+
+//Help section
+function showInstructions() {
+    "use strict";
+    document.getElementById("howtoplay").style.visibility = "visible";
+    document.getElementById("overlay").style.visibility = "visible";
+    buttonPress("help");
+}
+
+function closeInstructions() {
+    "use strict";
+    document.getElementById("howtoplay").style.visibility = "hidden";
+    document.getElementById("overlay").style.visibility = "hidden";
 }
